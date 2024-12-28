4143698173f89809cf39a72839379ee2dcaf5abd
machariamarigi <machariamarigi@gmail.com>
2024-12-28 23:28:58 +0300

    setup producer

---
+++ src/kafka/producer.ts
@@ -0,0 +1,20 @@
+import { KafkaClient, Producer } from "kafka-node";
+
+export const sendMessage = async (topic: string, message: string) => {
+  const client = new KafkaClient({ kafkaHost: "localhost:9092" });
+  const producer = new Producer(client);
+
+  producer.on("ready", () => {
+    producer.send([{ topic, messages: message }], (err, data) => {
+      if (err) {
+        console.error("Error sending message: " + err);
+      } else {
+        console.log("Message sent successfully", data);
+      }
+    });
+  });
+
+  producer.on("error", (err) => {
+    console.error("Error: " + err);
+  });
+};
